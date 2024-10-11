5155a89cbbfb5fbf57bd4cf80e67bcce16d7ec02
YoshiakiAsahara <asa-yossan@outlook.jp>
2024-10-11 14:15:52 +0900

    移した

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,12 @@
-export default function Home() {
-  return (
-    <div>
-      <p>こんにちは</p>
-    </div>
-  );
-}
+import getFeedback from "./actions/getFeedback";
+import Home from "./components/Home";
+
+const page = async () => {
+  const feedbacks = await getFeedback();
+  console.log(feedbacks);
+  if (!feedbacks) return <div>loading...</div>;
+
+  return <Home feedbacks={feedbacks} />;
+};
+
+export default page;
