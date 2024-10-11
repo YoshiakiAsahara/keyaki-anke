dccc06f5d4e5687250951ec815065df38d3d70f4
YoshiakiAsahara <asa-yossan@outlook.jp>
2024-10-11 13:12:10 +0900

    api作成

---
+++ app/api/feedback/rout.ts
@@ -0,0 +1,33 @@
+import { NextRequest, NextResponse } from "next/server";
+import prisma from "../../../lib/prisma";
+
+//GET API
+export async function GET(req: NextRequest) {
+  try {
+    const feedbacks = await prisma.feedback.findMany({
+      orderBy: {
+        createdAt: "desc",
+      },
+    });
+    return NextResponse.json(
+      { message: "Success", feedbacks },
+      { status: 200 }
+    );
+  } catch (err) {
+    return NextResponse.json({ message: "Error", err }, { status: 500 });
+  }
+}
+
+// FEEDBACk投稿用API
+export async function POST(req: NextRequest) {
+  try {
+    const { comment, createdAt } = await req.json();
+
+    const feedback = await prisma.feedback.create({
+      data: { comment, createdAt },
+    });
+    return NextResponse.json({ message: "Success", feedback }, { status: 201 });
+  } catch (err) {
+    return NextResponse.json({ message: "Error", err }, { status: 500 });
+  }
+}
