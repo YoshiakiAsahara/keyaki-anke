8ce97e37bca112b93b9219070d2276c826929198
YoshiakiAsahara <asa-yossan@outlook.jp>
2024-10-14 13:30:43 +0900

    log出力を消去

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,6 @@ import Home from "./components/Home";
 
 const page = async () => {
   const feedbacks = await getFeedback();
-  console.log(feedbacks);
   if (!feedbacks) return <div>loading...</div>;
   return <Home feedbacks={feedbacks} />;
 };
